5bbfb1030b85ef399490cf4183fcff8ceff0bba8
Change-Bae <ceun21mi@gmail.com>
2026-01-07 17:12:51 +0900

    Hello Github

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-{
-  "message" : "Hello world"
-}
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Github
         </p>
         <a
           className="App-link"
