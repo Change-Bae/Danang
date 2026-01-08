0478b3626f022d244668c4911931834554f2d16d
Change-Bae <ceun21mi@gmail.com>
2026-01-08 11:02:10 +0900

    Create login page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import './Login.css';
+import { FaUser, FaLock } from "react-icons/fa";
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <div className= "wrapper">
+      <div className="form-box login">
+        <form action={""}>
+          <h1>Login</h1>
+          <div className="input-box">
+            <input type="id" placeholder='ID' required />
+            <FaUser className="icon" />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder='PASSWORD' required />
+            <FaLock className="icon" />
+          </div>
+
+          <div className="forgot">
+            <label><input type="checkbox" />Forgot Password</label>
+          </div>
+          
+          <button type="submit">Login</button>
+
+        </form>
+      </div>
+    </div>
   )
 };
 
